dd8b347fdaa0bc94d3219eb2325a9a0b913a9ece
rumbledore <moshedota@gmail.com>
2019-10-15 15:07:21 +0300

    update main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ my_net = '192.168.2.0/24'
 
 def discover_IP_in_net():
     os.system('sudo apt install nmap -y')
-    os.system('sudo nmap -sS -O ' + my_net)
+    os.system('sudo nmap -sP ' + my_net)
 
 
 def Install_centos_packages():
